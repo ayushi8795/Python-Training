39ce056c2296726d394e43b12eaab2f0ab652e1b
Ayushi Agrawal <44095685+ayushi8795@users.noreply.github.com>
2021-03-15 13:45:00 -0400

    Add files via upload

---
+++ FIRSTPROJECT/FIRSTAPPLICATION/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from .models import Name,Id1,Address,contact
+# Register your models here.
+
+admin.site.register(Name)
+admin.site.register(Id1)
+admin.site.register(Address)
+admin.site.register(contact)
