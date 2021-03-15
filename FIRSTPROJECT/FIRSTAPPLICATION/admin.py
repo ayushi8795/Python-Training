9e1287418165de2dd00d1f4274694e5d35ee0a07
Ayushi Agrawal <44095685+ayushi8795@users.noreply.github.com>
2021-03-15 13:45:29 -0400

    Delete FIRSTPROJECT directory

--- FIRSTPROJECT/FIRSTAPPLICATION/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import Name,Id1,Address,contact
-# Register your models here.
-
-admin.site.register(Name)
-admin.site.register(Id1)
-admin.site.register(Address)
-admin.site.register(contact)
--- FIRSTPROJECT/FIRSTAPPLICATION/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class FirstapplicationConfig(AppConfig):
-    name = 'FIRSTAPPLICATION'
