3235bb5d791d029648570408cd8dd2ee915d4e8d
Ayushi Agrawal <44095685+ayushi8795@users.noreply.github.com>
2021-03-19 22:08:13 -0400

    Delete Crudapp/Blog_Post directory

--- Crudapp/Blog_Post/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from Blog_Post.models import BlogPost
-# Register your models here.
-
-admin.site.register(BlogPost)
--- Crudapp/Blog_Post/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('delete/<int:pk>/', views.Post_delete, name="post_delete"),
-    path('postform/', views.Post_form, name="post_new"),
-    path('', views.Post_list, name='Post_view'),
-
-]
